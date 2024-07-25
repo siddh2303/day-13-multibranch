117be1abc326ab975c0b019ed1d5c4c8038b2652
siddh2303 <siddhpatel2303@gmail.com>
2024-07-25 15:40:51 +0530

    Java app for Production branch

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World from Staging branch");
+        System.out.println("Hello World from production branch");
     }
 }
