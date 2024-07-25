844b51d9a7d2925627a4e39fc0c6f7641a3373c1
siddh2303 <siddhpatel2303@gmail.com>
2024-07-25 15:39:17 +0530

    Java app for Staging branch

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World from Development branch");
+        System.out.println("Hello World from Staging branch");
     }
 }
